def5d9e5359f3eb2d298fb19173b7cf1a84d9e7f
bryhan360 <blaciar.lonsallez@gmail.com>
2017-03-17 03:14:02 +0100

    Add files via upload

---
+++ ejercicio-mayor.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+valor1 = (input('introduzca Valor1: '))
+valor2 = (input('introduzca Valor2: '))
+
+	
+		if  valor1 > valor2  :
+			print 'valor1 es mayor '
+		
+			if  valor1 == valor2  :
+			print 'los 2 valores son iguales '
+			
+				else : 
+				print 'valor2 es mayor '
+				
---
+++ ejercicio-menu-calc.py
@@ -0,0 +1,36 @@
+# !/usr/bin/python
+# -*-coding: utf-8-*-
+
+import os
+
+	numero = int(raw_input('Introduce un numero ==> '))
+	numero2 = int(raw_input('Introduce otro numero ==> '))
+
+		print 'Que operacion desea hacer amo?'
+		print '0) Salir'
+		print '1) Sumar'
+		print '2) Restar'
+		print '3) Multiplicar'
+		print '4) Dividir'
+
+
+		opcion = raw_input('Elija una opcion ==> ')
+		if opcion == '0':
+			print 'Saliendo...'
+			print 'Adios!!!'
+
+		if opcion == '1':
+			suma = numero + numero2
+			print 'El resultado es ', suma
+	
+		if opcion == '2':
+			resta = numero - numero2
+			print 'El resultado es ', resta
+
+		if opcion == '3':
+			multi = numero * numero2
+			print 'El resultado es ', multi
+
+		if opcion == '4':
+			divi = numero / numero2
+			print 'El resultado es ', divi
